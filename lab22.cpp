4c37d177f796cbfc4b6bb06bb63b3c1c1ab91f10
19mddill <mddillshadul98@gmail.com>
2025-01-30 15:58:53 +0600

    Create lab22.cpp

---
+++ lab22.cpp
@@ -0,0 +1,68 @@
+
+#include<bits/stdc++.h>
+#include<graphics.h>
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+
+using namespace std;
+
+int main(){
+    int gd = DETECT,gm;
+    initgraph(&gd,&gm,"");
+    int maxX = getmaxx();
+    int maxY = getmaxy();
+    int x1 = 1;
+    int y1 = 1;
+    int x2 = 500;
+    int y2 = 500;
+    setcolor(GREEN);
+    line(maxX/2,0,maxX/2,maxY); // X axis
+    line(0,maxY/2,maxX,maxY/2); // Y axis
+    float m = (float)(y2 - y1)/(float)(x2 - x1);
+    float b = y2 - m * x2;
+    cout<<m<<' '<<b<<endl;
+    int x,y;
+    int xx = maxX/2;
+    int yy = maxY/2;
+    if(x1 == x2){
+        x = x1;
+        y = y1;
+        while(y <= yy - y2){
+            putpixel(x + xx, yy - y,GREEN);
+            y++;
+        }
+    }
+    else if(m == 0){
+        x = x1;
+        y =y1;
+        while(x <= x2){
+            putpixel(x + xx,yy - y,GREEN);
+            x++;
+        }
+    }
+    else if(abs(m) <=1){
+        x = x1;
+        y = y1;
+        while(x<=x2){
+            putpixel(x + xx,yy - y,GREEN);
+            x = x + 1;
+            y = m*x +b;
+        }
+    }
+    else if(abs(m) > 1){
+        x = x1;
+        y = y1;
+        while(y <=y2){
+            putpixel(x + xx,yy - y,GREEN);
+            y = y + 1;
+            x = (y - b)/m;
+        }
+    }
+
+
+    getch();
+    closegraph();
+
+    return 0;
+}
